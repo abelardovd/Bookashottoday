69c691047e1bca7f09f5f18a4f7762de718228d6
abelardo <ivo_v97@hotmail.com>
2018-10-02 20:52:02 -0500

    example 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,69 +6,89 @@ import Layout from '../components/layout'
 
 import imgWedding1 from '../images/imgWeddings.jpg'
 import imgEngagements2 from '../images/imgEngagements.jpg'
-
+import imgPortrait3 from '../images/imgPortraits.jpg'
 
 
 const Title = styled.h1`
-  font-size: 4.5em;
+  font-size: 3.2em;
   text-align: center;
   color:  #404040;
   position: absolute;
-  left: 300px;
-    top: 0px;
+  left: 330px;
+    top: -5px;
 `
 const Par1 = styled.h1`
 padding: 100px 15px;
 
-  font-size: 0.9em;
+  font-size: 5.9em;
   text-align: center;
-  color: black;
+  color: whitesmoke;
   position: absolute;
-    left: 210px;
-    top: 180px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: black;
-    width: 30%;
-    height: 40%;
-    z-index: -1;
+    left: 100px;
+    top: 110px;
+    
+    
+    z-index: 1;
 `
 
 const Par2 = styled.h1`
 padding: 100px 15px;
 
-  font-size: 0.9em;
+  font-size: 5.9em;
   text-align: center;
-  color: black;
+  color: whitesmoke;
   position: absolute;
-    left: 650px;
+    left: 330px;
     top: 550px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: black;
-    width: 30%;
-    height: 50%;
-    z-index: -1;
+   
+    
+    z-index: 1;
+`
+const Par3 = styled.h1`
+padding: 100px 15px;
+
+  font-size: 5.9em;
+  text-align: center;
+  color: whitesmoke;
+  position: absolute;
+    left: 40px;
+    top: 1150px;
+   
+    
+    z-index: 1;
 `
 
 const Image = styled.img`
     background-color: #dcdcd4;
     margin-left: auto;
     margin-right: auto;
-    width: 36%;
+    width: 100%;
+    height: 60%;
     position: absolute;
-    left: 500px;
-    top: 200px;
+    left: 0px;
+    top: 50px;
 `
 
 const Image2 = styled.img`
     background-color: #dcdcd4;
     margin-left: auto;
     margin-right: auto;
-    width: 36%;
+    width: 100%;
+    height: 60%;
     position: absolute;
-    left: 300px;
-    top: 600px;
+    left: 0px;
+    top: 550px;
+`
+
+const Image3 = styled.img`
+    background-color: #dcdcd4;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    height: 60%;
+    position: absolute;
+    left: 0px;
+    top: 1000px;
 `
 const Button = styled.a`
 
@@ -122,13 +142,14 @@ display: inline-block;
 const IndexPage = () => (
   
   <Layout>
-    <Title>CURRENT DEALS</Title>
+    <Title>Book A Shoot Today</Title>
     <Image src={imgWedding1} alt='something here'/>
-    <Par1>PROMO NAME HERE</Par1>
+    <Par1>weddings</Par1>
     <Image2 src={imgEngagements2} alt='something here'/>
-    <Par2>PROMO 2 NAME HERE</Par2>
-    <Button href="https://codepen.io/lhorainemarinas/full/qmyvoQ/">BUY NOW</Button> 
-    <Button2 href="https://codepen.io/lhorainemarinas/full/qmyvoQ/">BUY NOW</Button2>
+    <Par2>Engagements</Par2>
+    <Image3 src={imgPortrait3} alt='something here'/>
+    <Par3>Portait</Par3>
+    
     
     
     
