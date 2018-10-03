c284d02cb160dd2792b5a8499ea1287168a4acdd
abelardo <ivo_v97@hotmail.com>
2018-10-02 20:06:39 -0500

    gatsby develop

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/yadiraeloisadavilacorona/Desktop/Trabajo/Bookashottoday/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/yadiraeloisadavilacorona/Desktop/Trabajo/Bookashottoday/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
